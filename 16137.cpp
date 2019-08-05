934dc3835cd825eb40be377628a5df5fdb64421f
yanagi <40001772+ryujisuda@users.noreply.github.com>
2019-08-06 02:49:42 +0900

    Update 16137.cpp

--- 16137.cpp
+++ 16137.cpp
@@ -1,159 +1,125 @@
 //https://www.acmicpc.net/problem/16137
-/*풀이 : 오작교를 건널 수 있는 경우 : 위 아래가 땅 또는 왼쪽 오른쪽이 땅
-경계넘어서는 땅이라고 생각한다. 오작교를 건넌 경우면
-flag 값을 true로 하고 또 오작교가 나오면 큐에다가 push 해주지 않는다.
-만약 (0,0)이 오작교면 그 오작교는 건넌거라고 생각하지 않는다.(상하좌우가 오작교여도 갈 수 있음)*/
+/*풀이 : 오작교를 건널 수 있는 경우 : 위 아래가 절벽 하나 이상, 왼쪽 오른쪽이 절벽 하나 이상이 아닌 경우. 
+오작교를 건넌 경우면
+hi 값을 true로 하고 또 오작교가 나오면 큐에다가 push 해주지 않는다.*/
 #include <iostream>
 #include <algorithm>
-#include <vector>
 #include <queue>
-#include <cstring>
-
+#include <vector>
 using namespace std;
-
 typedef pair<int, int> pii;
-
-
+int n, m, o;
 int arr[11][11];
 int dx[4] = { -1,1,0,0 };
 int dy[4] = { 0,0,-1,1 };
-bool visit[11][11];
-int n, m;
-vector <pii> v;
-bool hi;
-
-int bfs()
+vector<pii> v;
+int minn = 1e9;
+struct point
 {
-	memset(visit, 0, sizeof(visit));
-	int time = 0;
-	queue<pii> q;
-	q.push({ 0,0 });
+	int x, y;
+	bool hi;//오작교 건너고 온건지 아닌지 체크
+};
+void bfs()
+{
+	int cnt = 0;
+	bool visit[11][11] = { 0, };
+	queue<point> q;
+	q.push({ 0,0,0 });
 	visit[0][0] = true;
-
-	if (arr[0][0] >= 2)
-	{
-		time = arr[0][0];
-	}
 	while (!q.empty())
 	{
 		int sizz = q.size();
 		while (sizz--)
 		{
-			pii front = q.front();
-			//cout << front.first << " " << front.second << '\n';
+			point front = q.front();
 			q.pop();
-			if (front.first == n - 1 && front.second == n - 1)
+			if (front.x == n - 1 && front.y == n - 1)
 			{
-				return time;
+				minn = min(minn, cnt);
+				return;
 			}
-			hi = false;
-			if (arr[front.first][front.second] >= 2
-				&& !(front.first == 0 && front.second == 0))
-			{
-				if (time%arr[front.first][front.second] != 0)
-				{
-					q.push({ front.first,front.second });
-					continue;
-				}
-				else
-					hi = true;
-			}
-
 			for (int i = 0; i < 4; i++)
 			{
-				int nx = front.first + dx[i];
-				int ny = front.second + dy[i];
-				if (nx >= 0 && nx < n && ny >= 0 && ny < n && visit[nx][ny] == false
-					&& arr[nx][ny] >= 1)
+				int nx = dx[i] + front.x;
+				int ny = dy[i] + front.y;
+				bool hi = front.hi;
+				if (nx >= 0 && nx < n && ny >= 0 && ny < n && arr[nx][ny] >= 1 && !visit[nx][ny])
 				{
-					if (hi == true && arr[nx][ny] != 1)
-						continue;
-					q.push({ nx,ny });
-					visit[nx][ny] = true;
+					if (arr[nx][ny] == 1)
+					{
+						q.push({ nx,ny,0 });
+						visit[nx][ny] = true;
+					}
+					else if (arr[nx][ny] >= 2)
+					{
+						if (((cnt + 1) % arr[nx][ny] == 0) && (hi == false))
+						{
+							q.push({ nx,ny,1 });
+							visit[nx][ny] = true;
+						}
+						else if ((cnt + 1) % arr[nx][ny] != 0 && (hi == false))
+						{
+							q.push({ front.x,front.y,hi });
+						}
+					}
 				}
 			}
 		}
-		time++;
+		cnt++;
 	}
-	return 1e9;
+
 }
-
-
 int main()
 {
 	ios_base::sync_with_stdio(0);
-
+	cin.tie(0);
+	cout.tie(0);
 	cin >> n >> m;
-
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < n; j++)
 		{
 			cin >> arr[i][j];
+
 		}
 	}
-
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < n; j++)
 		{
-			//가능한 좌표를 넣는다. 위 아래가 전부 땅이거나 왼쪽 오른쪽이 전부 땅이면 가능한 좌표
 			if (arr[i][j] == 0)
 			{
-				int cnt = 0;
-				for (int k = 0; k < 2; k++)
+				int cnt1 = 0;
+				int cnt2 = 0;
+				for (int r = 0; r < 2; r++)
 				{
-					int nx = i + dx[k];
-					int ny = j + dy[k];
-
-					if (!(nx >= 0 && nx < n&&ny >= 0 && ny < n))
-					{
-						cnt++;
-						continue;
-					}
-					else if (arr[nx][ny] == 1)
-					{
-						cnt++;
-					}
+					int nx = dx[r] + i;
+					int ny = dy[r] + j;
+					if (nx >= 0 && nx < n && ny >= 0 && ny < n && arr[nx][ny] == 0)
+						cnt1++;
 				}
-				if (cnt == 2)
+				for (int r = 2; r < 4; r++)
 				{
-					v.push_back({ i,j });
-					continue;
+					int nx = dx[r] + i;
+					int ny = dy[r] + j;
+					if (nx >= 0 && nx < n && ny >= 0 && ny < n && arr[nx][ny] == 0)
+						cnt2++;
 				}
-				cnt = 0;
-				for (int k = 2; k < 4; k++)
+				if (!(cnt1 >= 1 && cnt2 >= 1))
 				{
-					int nx = i + dx[k];
-					int ny = j + dy[k];
-
-					if (!(nx >= 0 && nx < n&&ny >= 0 && ny < n))
-					{
-						cnt++;
-						continue;
-					}
-					else if (arr[nx][ny] == 1)
-					{
-						cnt++;
-					}
-				}
-				if (cnt == 2)
-				{
-					v.push_back({ i,j });
+					v.push_back({ i,j });//오작교 위치 후보
 				}
 			}
 		}
 	}
-	int minn = 1e9;
+
 	for (int i = 0; i < v.size(); i++)
 	{
-		pii front = v[i];
-		arr[front.first][front.second] = m;
-		int num = bfs();
-		minn = min(minn, num);
-		arr[front.first][front.second] = 0;
+		arr[v[i].first][v[i].second] = m;
+		bfs();
+		arr[v[i].first][v[i].second] = 0;
 	}
 	cout << minn;
-
 	return 0;
 }
+
