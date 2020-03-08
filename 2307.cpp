03eb5c7c4f2d1d2b682c9d90ed942fc14f19b890
ryuspace <40001772+ryuspace@users.noreply.github.com>
2020-03-08 18:40:20 +0900

    Update 2307.cpp

--- 2307.cpp
+++ 2307.cpp
@@ -1,86 +1,117 @@
 //https://www.acmicpc.net/problem/2307
 /*풀이 : 전체에 대해서 dikstra를 돌리고 dikstra 경로를 역추적 해서 경로를 하나씩 지워나가면서 지연시간의 최대를 찾는다. 최단 경로로 갈 것이고 최단 경로가 아닌 곳을 지우면 의미가 없다.*/
 #include <iostream>
-#include <queue>
+#include <algorithm>
 #include <vector>
-#include <functional>
-#include <cstring>
+#include <queue>
+
 using namespace std;
 
-int V, E;
-int k;
+typedef pair<int, int> pii;
 
-vector < pair<int, int> > vec[1001];
+int n, m;
+vector<pii> v[1001];
+int price[1001];
+
 int parent[1001];
-priority_queue< pair<int, int>, vector<pair<int, int> >, greater<pair<int, int> > > q;
+int minn;
+int minn2 = 0;
 
-int price[20001];
-
-int dikstra(int u,int v) //u에서 v로 가는 방법을 막는다.
+void dik()
 {
-	for (int i = 1; i <= V; i++)
-	{
-		price[i] = 1e9;
-	}
-	price[1] = 0;
-	q.push({ 0,1 });
+    for (int i = 1; i <= n; i++)
+    {
+        price[i] = 1e9;
+    }
+    priority_queue<pii, vector<pii>, greater<pii> > q;
+    q.push({ 1,0 });
+    price[1] = 0;
+    while (!q.empty())
+    {
+        pii front = q.top();
+        q.pop();
+        int now = front.first;
+        int sum = front.second;
 
-	while (!q.empty())
-	{
-		int node = q.top().second;
-		int pr = q.top().first;
-		q.pop();
-		if (price[node] < pr)
-			continue;
-		for (int i = 0; i < vec[node].size(); i++)
-		{
-			int next_node = vec[node][i].first;
-			int next_price = vec[node][i].second;
-			if ((node == u && next_node == v) || (node == v && next_node == u))
-			{
-				continue;
-			}
-			if (next_price + pr < price[next_node])
-			{
-				price[next_node] = next_price + pr;
-				q.push({ next_price + pr,next_node });
-				parent[next_node] = node;
-			}
-		}
-	}
-	return price[V];
+        for (int i = 0; i < v[now].size(); i++)
+        {
+            if (price[v[now][i].first] > price[now] + v[now][i].second)
+            {
+                parent[v[now][i].first] = now;
+                price[v[now][i].first] = price[now] + v[now][i].second;
+                q.push({ v[now][i].first,price[now] + v[now][i].second });
+            }
+        }
+    }
+}
+
+void dik2(int before, int next)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        price[i] = 1e9;
+    }
+    priority_queue<pii, vector<pii>, greater<pii> > q;
+    q.push({ 1,0 });
+    price[1] = 0;
+    while (!q.empty())
+    {
+        pii front = q.top();
+        q.pop();
+        int now = front.first;
+        int sum = front.second;
+        for (int i = 0; i < v[now].size(); i++)
+        {
+            if ((before == now && next == v[now][i].first) || (before == v[now][i].first && next == now))
+            {
+                continue;
+
+            }
+            if (price[v[now][i].first] > price[now] + v[now][i].second)
+            {
+                price[v[now][i].first] = price[now] + v[now][i].second;
+                q.push({ v[now][i].first,price[now] + v[now][i].second });
+            }
+        }
+    }
 
 }
-int main()
+int main(void)
 {
-	ios_base::sync_with_stdio(0);
-	std::cin.tie(0);
-	std::cout.tie(0);
-	memset(parent, -1, sizeof(parent));
-	cin >> V >> E;
-	for (int i = 0; i<E; i++)
-	{
-		int a, b, c;
-		cin >> a >> b >> c;
-		vec[a].push_back({ b,c });
-	}
-	int Minn=dikstra(0,0);
-	int vertex = V;
-	int ans = 0;
-	while (parent[vertex] != -1)
-	{
-		int u = vertex;
-		int v = parent[u];
-		ans = max(ans, dikstra(u, v));
-		vertex = v;
-	}
-	if (ans == 1e9)
-	{
-		cout << -1;
-	}
-	else
-	{
-		cout << ans - Minn;
-	}
-	return 0;
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n >> m;
+    for (int i = 0; i < m; i++)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        v[a].push_back({ b,c });
+        v[b].push_back({ a,c });
+    }
+    dik();
+    minn = price[n];
+    int _n = n;
+    while (_n != 1)
+    {
+
+        int before = _n;
+        int next = parent[_n];
+        dik2(before, next);
+        if (price[n] == 1e9)
+        {
+            cout << -1;
+            exit(0);
+        }
+        else
+        {
+            minn2 = max(minn2, price[n] - minn);
+        }
+        _n = parent[_n];
+    }
+    cout << minn2;
+    return 0;
+
 }
