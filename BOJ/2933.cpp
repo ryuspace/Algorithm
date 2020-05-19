e6f8012a878997c580e60b4e2c4a0092e0ab6363
ryuspace <40001772+ryuspace@users.noreply.github.com>
2020-05-19 11:50:22 +0900

    Update 2933.cpp

--- BOJ/2933.cpp
+++ BOJ/2933.cpp
@@ -3,124 +3,177 @@
 클러스터 하나가 떨어질 수 밖에 없다. 원래 다 이어졌으니까 
 핵심 : 맨 아래 첫번째 줄을 bfs 돌리면 밑에 큰 클러스터가 인식이 된다. 방문되지
 않은 'x'는 클러스터가 아님. 밑으로 떨어뜨려야 한다. 합칠때는 미네랄과 클러스터의
-열의 차이의 최소만큼 갈 수 있다. 어려웠다!!*/
+열의 차이의 최소만큼 갈 수 있다. 어려웠다!!
+*/
 #include <iostream>
 #include <algorithm>
 #include <queue>
 #include <cstring>
+
 using namespace std;
 
 typedef pair<int, int> pii;
-
+int r, c, n;
 char arr[101][101];
 bool visit[101][101];
+
 int dx[4] = { -1,1,0,0 };
 int dy[4] = { 0,0,-1,1 };
-int r, c;
-
-void bfs(pii start)
+void bfs(int rr,int cc)
 {
 	queue<pii> q;
-	q.push({ start.first,start.second });
-	visit[start.first][start.second] = true;
+	q.push({ rr,cc });
+	visit[rr][cc] = true;
+
 	while (!q.empty())
 	{
 		pii front = q.front();
 		q.pop();
 		for (int i = 0; i < 4; i++)
 		{
-			int nx = front.first + dx[i];
-			int ny = front.second + dy[i];
-			if (nx >= 0 && nx < r && ny >= 0 && ny < c
-				&&visit[nx][ny] == false && arr[nx][ny] == 'x')
+			int nx = dx[i] + front.first;
+			int ny = dy[i] + front.second;
+			if (nx >= 0 && nx < r && ny >= 0 && ny < c && !visit[nx][ny] && arr[nx][ny] == 'x')
 			{
 				q.push({ nx,ny });
 				visit[nx][ny] = true;
 			}
 		}
 	}
-}
-void Union()//땅바닥 부터 시작해서 클러스터를 찾자 ☆ 여기서 안 찾아진 애들은 따로 노는애들
-{
 
-	memset(visit, false, sizeof(visit));
-	for (int i = 0; i < c; i++)
+
+}
+void check(int num,int dir)
+{
+	memset(visit, 0, sizeof(visit));
+	int high = r - num;
+
+	if (dir == 0)
 	{
-		if (visit[r - 1][i] == false && arr[r - 1][i] == 'x')
+		int cnt = 0;
+		while (arr[high][cnt] != 'x' && cnt < c)
 		{
-			bfs({ r - 1,i });
+			cnt++;
 		}
-	}
-	int down = 1e9;
-	for (int i = 0; i <c; i++)
-	{
-		for (int j = r-1; j>=0; j--)
+		if (cnt < c)
 		{
-			if (arr[j][i] == 'x' && visit[j][i] == false)
+			arr[high][cnt] = '.';
+			for (int i = 0; i < c; i++)
 			{
-				int now = j+1;
-				while (arr[now][i] == '.' && now <r)
+				if (arr[r - 1][i] == 'x' && !visit[r-1][i])
 				{
-					now++;
+					bfs(r - 1, i);
 				}
-				down = min(now -j-1, down);//얼마나 내려가야하는지.
-				break;
-
 			}
-		}
-	}
-	for (int i = r - 1; i >= 0; i--)
-	{
-		for (int j = 0; j <c; j++)
-		{
-			if (i + down<r && down != 0 && arr[i][j] == 'x' && visit[i][j] == false)
+			int mini = 1e9;
+			for (int i = 0; i < c; i++)
 			{
+				for (int j = r - 2; j >= 0; j--)
+				{
+					if (!visit[j][i] && arr[j][i] == 'x')
+					{
+						int start = j + 1;
+						int cnt = 0;
+						while (start < r && arr[start][i] == '.')
+						{
+							cnt++;
+							start++;
+						}
+						if (start < r && visit[start][i] == false)
+							break;
+						mini = min(mini, cnt);
 
-				arr[i + down][j] = 'x';
-				arr[i][j] = '.';
+					}
+				}
+			}
+			if (mini >= 1e9)
+				return;
 
+			for (int i = r - 1; i >= 0; i--)
+			{
+				for (int j = 0; j < c; j++)
+				{
+					if (!visit[i][j] && arr[i][j] == 'x')
+					{
+
+						arr[i][j] = '.';
+						arr[i + mini][j] = 'x';
+
+					}
+				}
 			}
 		}
-	}
-
-
-}
-void destroy(int num, int turn)
-{
-
-	if (turn == 0)
-	{
-		int y = 0;
-		while (arr[num][y] != 'x' && y<c)
+		else
 		{
-			y++;
-		}
-		if (y != c)//부딪혔었다.
-		{
-			arr[num][y] = '.';
-			Union();
+			return;
 		}
 	}
 	else
 	{
-		int y = c - 1;
-		while (arr[num][y] != 'x' && y >= 0)
+		int cnt = c-1;
+		while (arr[high][cnt] != 'x' && cnt >=0)
 		{
-			y--;
+			cnt--;
 		}
-		if (y != -1)//부딪혔었다.
+		if (cnt >=0)
 		{
-			arr[num][y] = '.';
-			Union();
+			arr[high][cnt] = '.';
+			for (int i = 0; i < c; i++)
+			{
+				if (arr[r - 1][i] == 'x' &&!visit[r-1][i])
+				{
+					bfs(r - 1, i);
+				}
+			}
+			int mini = 1e9;
+			for (int i = 0; i < c; i++)
+			{
+				for (int j = r - 2; j >= 0; j--)
+				{
+					if (!visit[j][i] && arr[j][i] == 'x')
+					{
+						int start = j + 1;
+						int cnt = 0;
+						while (start < r && arr[start][i] == '.')
+						{
+							cnt++;
+							start++;
+						}
+						if (start < r && visit[start][i] == false)
+							break;
+						mini = min(mini, cnt);
+					}
+				}
+			}
+
+			if (mini >= 1e9)
+				return;
+			for (int i = r-1; i>=0; i--)
+			{
+				for (int j = 0; j < c; j++)
+				{
+					if (!visit[i][j] && arr[i][j] == 'x')
+					{
+
+						arr[i][j] = '.';
+						arr[i + mini][j] = 'x';
+
+					}
+				}
+			}
+		}
+		else
+		{
+			return;
 		}
 	}
-
 }
+
 int main()
 {
-	std::ios_base::sync_with_stdio(0);
-	std::cin.tie(0);
-	std::cout.tie(0);
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
 
 	cin >> r >> c;
 
@@ -132,21 +185,14 @@ int main()
 		}
 	}
 
-	int t;
-	cin >> t;
-	for (int i = 0; i < t; i++)
+	cin >> n;
+	for (int i = 0; i < n; i++)
 	{
 		int a;
 		cin >> a;
-		if (i % 2 == 0)//창영
-		{
-			destroy(r - a, 0);
-		}
-		else//상근
-		{
-			destroy(r - a, 1);
-		}
+		check(a,i%2);
 	}
+
 	for (int i = 0; i < r; i++)
 	{
 		for (int j = 0; j < c; j++)
@@ -155,6 +201,5 @@ int main()
 		}
 		cout << '\n';
 	}
-
 	return 0;
 }
