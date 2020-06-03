d9bcf68cecbf3adbc37d8131363297ebb586fb3b
ryuspace <40001772+ryuspace@users.noreply.github.com>
2020-06-03 20:07:52 +0900

    Update 16988.cpp

--- BOJ/16988.cpp
+++ BOJ/16988.cpp
@@ -1,59 +1,55 @@
 //https://www.acmicpc.net/problem/16988
 /*풀이 :0인 칸 2개를 선택할 수 있는 가능한 모든 경우를 찾고
-각각의 경우에 bfs를 돌려서 잡을 수 있는 
+각각의 경우에 bfs를 돌려서 잡을 수 있는
 상대방의 돌의 개수의 최대값을 찾는다.
 시간 복잡도 : 돌을 2개를 놓는 경우 : (N^2*M^2)
 bfs 돌리는 경우 N*M
-총 N^3*M^3*/
+총 N^3*M^3
+
+큐를 전역 변수에 선언하니깐 시간이 4배 가까이가 줄었다.
+bfs가 잦게 실행되는데 큐를 지역변수로 선언하면 생각보다 오버헤드가 큰가보다.
+
+*/
 #include <iostream>
 #include <algorithm>
-#include <queue>
+#include <cstring>
 #include <vector>
+#include <queue>
 using namespace std;
+
 typedef pair<int, int> pii;
 int n, m;
 int arr[21][21];
-bool use[21][21];
-bool visit[21][21];
+vector<pii> v;
+bool visit[410];
+bool black[21][21];
+
 int dx[4] = { -1,1,0,0 };
 int dy[4] = { 0,0,-1,1 };
-int maxx = 0;
 
+int maxi;
 
-void init()
-{
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < m; j++)
-		{
-			visit[i][j] = false;
-		}
-	}
-}
+queue<pii> q;
 int bfs(pii start)
 {
 	bool flag = false;
-	int cnt = 1;
-	vector<pii> bye;
-	queue<pii> q;
+	black[start.first][start.second] = true;
 	q.push(start);
-	bye.push_back(start);
-	visit[start.first][start.second] = true;
+	int cnt = 1;
 	while (!q.empty())
 	{
 		pii front = q.front();
 		q.pop();
-		for (int i = 0; i < 4; i++)
+		for (int i = 0;i < 4;i++)
 		{
 			int nx = dx[i] + front.first;
 			int ny = dy[i] + front.second;
-			if (nx >= 0 && nx < n && ny >= 0 && ny < m )
+			if (nx >= 0 && nx < n && ny >= 0 && ny < m)
 			{
-				if (!visit[nx][ny]&&arr[nx][ny] == 2)
+				if (arr[nx][ny] == 2 && !black[nx][ny])
 				{
-					bye.push_back({ nx,ny });
-					visit[nx][ny] = true;
 					q.push({ nx,ny });
+					black[nx][ny] = true;
 					cnt++;
 				}
 				else if (arr[nx][ny] == 0)
@@ -64,60 +60,53 @@ int bfs(pii start)
 		}
 	}
 	if (flag)
-	{
 		return 0;
-	}
-	else
-		return cnt;
+	return cnt;
 }
 
-void dfs(int idx,int cnt)
-{
-	if (cnt == 2)
-	{
-		int cntt = 0;
-		for (int i = 0; i < n; i++)
-		{
-			for (int j = 0; j < m; j++)
-			{
-				if (!visit[i][j] && arr[i][j] == 2)
-				{
-					int num = bfs({ i,j });
-					cntt+= num;
-				}
-			}
-		}
-		init();
-		maxx = max(cntt, maxx);
-		return;
-	}
-	for (int i = idx; i < n; i++)
-	{
-		for (int j = 0; j < m; j++)
-		{
-			if (arr[i][j] == 0 && !use[i][j])
-			{
-				use[i][j] = true;
-				arr[i][j] = 1;
-				dfs(i, cnt + 1);
-				use[i][j] = false;
-				arr[i][j] = 0;
-			}
-		}
-	}
-}
 int main()
 {
+	//freopen("Test.txt", "r", stdin);
 	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
 	cin >> n >> m;
-	for (int i = 0; i < n; i++)
+	for (int i = 0;i < n;i++)
 	{
-		for (int j = 0; j < m; j++)
+		for (int j = 0;j < m;j++)
 		{
 			cin >> arr[i][j];
+			if (arr[i][j] == 0)
+				v.push_back({ i,j });
 		}
 	}
-	dfs(0,0);
-	cout << maxx;
+
+	for (int i = 0;i < v.size() - 1;i++)
+	{
+		arr[v[i].first][v[i].second] = 1;
+		for (int j = i + 1;j < v.size();j++)
+		{
+			arr[v[j].first][v[j].second] = 1;
+			int sum = 0;
+			memset(black, 0, sizeof(black));
+			for (int r = 0;r < n;r++)
+			{
+				for (int k = 0;k < m;k++)
+				{
+					if (!black[r][k] && arr[r][k] == 2)
+					{
+						sum += bfs({ r,k });
+					}
+				}
+			}
+			maxi = max(maxi, sum);
+			arr[v[j].first][v[j].second] = 0;
+
+		}
+		arr[v[i].first][v[i].second] = 0;
+	}
+	cout << maxi;
+
 	return 0;
 }
