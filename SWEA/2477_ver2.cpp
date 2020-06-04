96e9477343cbcabd05bbd5ff6c70883390e348f2
ryuspace <40001772+ryuspace@users.noreply.github.com>
2020-06-04 21:26:15 +0900

    Update 2477_ver2.cpp

--- SWEA/2477_ver2.cpp
+++ SWEA/2477_ver2.cpp
@@ -1,156 +1,144 @@
 /*2477번 차량정비소
 풀이 : 시키는대로 구현하자
 ver1보다 훨씬 현실적인 상황을 고려해서 코딩했다. 시간과 코드 길이는 더 늘어났지만 디버깅하기가 더 편했다.
-주석으로 추가 설명을 하겠습니다.
-
-간과했던 부분 : 마지막 인덱스 사람이 마지막으로 끝날 것이라고 생각했다. + 문제를 잘 읽자.
 */
 #include <iostream>
 #include <algorithm>
 #include <queue>
-#include <vector>
-#include <functional>
+#include <cstring>
 using namespace std;
+
 typedef pair<int, int> pii;
-int t, n, m, k, a,b;
-struct info
-{
-	int jin, so, idx;
-};
-//접수 창구와 정비 창구가 갖고 있는 요소는 진행 시간, 접수(정비)창구의 소요시간, 그 창구에 있는 사람 
+
+int t,n,m,k,a,b;
+
+int rec_time[10];
+int rep_time[10];
 struct point
 {
-	int time, num, idx;
-};//정비창구의 우선순위 요소
-bool cmp(const point& a, const point& b)
+	int burst, who;
+};
+
+struct people
 {
-	if (a.time < b.time)
-		return true;
-	else if (a.time == b.time)
-	{
-		if (a.num < b.num)
-			return true;
-		else
-			return false;
-	}
-	else
-		return false;
-}
-info jub[11];
-info jung[11];
+	int idx, time;
+};
+point rec[10];
+point rep[10];
+
+queue<people> q1;
+
+
+pii check[1001];
 
 int main()
 {
-	freopen("Text.txt","r", stdin);
+	//freopen("Test.txt", "r", stdin);
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
-	cin >> t;
-	for (int r = 1; r <= t; r++)
-	{
-		int out = 0;
-		pii ans[1001];
-		queue<pii> zero;
-		priority_queue<pii, vector<pii>, greater<pii> > one;
-		vector<point> two;
-		cin >> n >> m >> k >> a>>b;
-		for (int i = 1; i <= n; i++)
-		{
-			cin >> jub[i].so;
-			jub[i].jin = 0;
-		}
-		for (int i = 1; i <= m; i++)
-		{
-			cin >> jung[i].so;//진행시간 소요시간 있는 사람
-			jung[i].jin = 0;
 
-		}
-		for (int i = 1; i <= k; i++)
+	cin >> t;
+	for (int q = 1;q <= t;q++)
+	{
+		memset(rep, 0, sizeof(rep));
+		vector<pii> v;
+		queue<int> q2;
+		cin >> n >> m >> k >> a >> b;
+		for (int i = 0;i < n;i++)
 		{
-			int n;
-			cin >> n;
-			zero.push({ i,n });//사람, 온 시간..
+			cin >> rec_time[i];
 		}
-		int idx = 1;
-		for (int time = 0;; time++)
+		for (int i = 0;i < m;i++)
 		{
-			while (!zero.empty() && time == zero.front().second)
+			cin >> rep_time[i];
+		}
+		for (int i = 0;i < k;i++)
+		{
+			int aa;
+			cin >> aa;
+			q1.push({ i + 1,aa });
+		}
+		int finish = 0;
+		for (int time = 0;;time++)
+		{
+			//먼저 끝났으면 내쫓기
+			for (int i = 0;i < n;i++)
 			{
-				one.push({ zero.front().first,zero.front().second });
-				zero.pop();
-			}
-			for (int i = 1; i <= n; i++)
-			{
-				if (jub[i].jin==jub[i].so)//시간이 다 지났다 내보내자.
+				if (rec[i].who != 0)
 				{
-					two.push_back({ time,i,jub[i].idx });
-					jub[i].idx= 0;
-					jub[i].jin = 0;
-				}
-			}
-			for (int i = 1; i <= n; i++)
-			{
-				if (jub[i].idx == 0)
-				{
-					if (!one.empty())
+					rec[i].burst--;
+					if (rec[i].burst == 0)
 					{
-						ans[one.top().first].first = i;
-						jub[i].idx = one.top().first;
-						jub[i].jin = 1;
-						one.pop();
+						q2.push({ rec[i].who });
+						rec[i].who = 0;
 					}
 				}
-				else
-				{
-					jub[i].jin++;
-				}
 			}
-			bool flag = false;
-			sort(two.begin(), two.end(),cmp);
-			for (int i =1; i <= m; i++)
+
+			if (!q1.empty())
 			{
-				if (jung[i].jin ==jung[i].so)
+				for (int i = 0;i < n;i++)
 				{
-					out++;
-					if (out == k)
-						flag = true;
-					jung[i].idx = 0;
-					jung[i].jin = 0;
-				}
-			}
-			for (int i = 1; i <= m; i++)
-			{
-				if (jung[i].idx == 0)
-				{
-					if (!two.empty())
+					if (!q1.empty() && q1.front().time <= time && rec[i].who == 0)
 					{
-						ans[two[0].idx].second = i;
-						jung[i].idx = two[0].idx;
-						jung[i].jin= 1;
-						two.erase(two.begin());
+						rec[i].who = q1.front().idx;
+						rec[i].burst = rec_time[i];
+						check[rec[i].who].first = i + 1;
+						q1.pop();
 					}
 				}
-				else
+			}
+			//접수창구 완료
+
+			
+			for (int i = 0;i < m;i++)
+			{
+				if (rep[i].who != 0)
 				{
-					jung[i].jin++;
+					rep[i].burst--;
+					if (rep[i].burst == 0)
+					{
+						rep[i].who = 0;
+					}
 				}
 			}
-			if (flag)
+
+			if (!q2.empty())
+			{
+				for (int i = 0;i < m;i++)
+				{
+					if (!q2.empty() && rep[i].who == 0)
+					{
+
+						rep[i].who = q2.front();
+						rep[i].burst = rep_time[i];
+						check[rep[i].who].second = i + 1;
+						q2.pop();
+						finish++;
+					}
+				}
+			}
+
+			if (finish == k)
 				break;
 		}
-		int cnt = 0;
-
-		for (int i = 1; i <= k; i++)
+		int sum = 0;
+		for (int i = 1;i <= k;i++)
 		{
-			if (ans[i].first ==  a && ans[i].second==b)
+
+			if (check[i].first == a && check[i].second == b)
 			{
-				cnt += i;
+				sum += i;
 			}
+
 		}
-		if (cnt == 0)
-			cnt = -1;
-		cout << "#" << r << " " << cnt << '\n';
+		if (sum == 0)
+		{
+			cout << "#" << q << " " << -1 << '\n';
+		}
+		else
+			cout << "#" << q << " " << sum << '\n';
 	}
 	return 0;
 }
-
