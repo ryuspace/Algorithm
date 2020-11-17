e6c7b52c2c5dd99f1375e924dbadfb627ffcb936
ryuspace <40001772+ryuspace@users.noreply.github.com>
2020-11-17 13:41:38 +0900

    Create upper_bound.cpp

---
+++ Preperation/upper_bound.cpp
@@ -0,0 +1,14 @@
+int upperBound(int target) //찾는 수가 없으면 n번째 인덱스 가리킴 (0~n-1)
+{
+	int le = 0;
+	int ri = n;
+	while (le < ri)
+	{
+		int mid = (le + ri) / 2;
+		if (arr[mid] > target)
+			ri = mid;
+		else
+			le = mid + 1;
+	}
+	return ri;
+}
