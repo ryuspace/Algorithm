43b56eb170a94e0929988a6d27c6cf6e7d422d83
ryuspace <40001772+ryuspace@users.noreply.github.com>
2021-02-15 14:20:41 +0900

    Create 하노이1.cpp

---
+++ Xpert/하노이1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+voidhanoi(intn, ints, inte){
+if(n <= 0) return;
+hanoi(n-1, s, 6-s-e); //n-1개의 하노이탑을 여분의 위치에 치워주세요
+printf("%d : %d -> %d\n", n, s, e);//그럼 가장 큰 하노이 하나가 남을 테니 걔를 3번째 위치에 치워주세요
+hanoi(n-1, 6-s-e, e); //n-1개의 하노이탑을 여분의 위치에서 3번째 위치로 치워주세요
+}
+intmain(){
+intN;
+scanf("%d", &N);
+hanoi(N, 1, 3);
+return0;
+}
