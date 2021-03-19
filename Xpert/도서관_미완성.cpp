dcd642a63d3b56c62b14aed09014b18b683ef376
ryuspace <40001772+ryuspace@users.noreply.github.com>
2021-03-20 00:07:11 +0900

    Update 도서관_미완성.cpp

--- Xpert/도서관_미완성.cpp
+++ Xpert/도서관_미완성.cpp
@@ -1,107 +1,17 @@
+#ifndef NULL
 #define NULL 0
+#endif // NULL
+
 #define MAXTITLE 10010
+#define MAXTITLELEN 22
+
 #define MAXAUTHOR 510
+#define MAXAUTHORLEN 12
+
 #define MAXGENRE 110
-typedef unsigned long long ull;
+#define MAXGENRELEN 12
 
-struct Node
-{
-    char title[22];
-    ull author;
-    ull genre;
-    int cnt;
-    int loan;
-};
-
-struct Title
-{
-    ull title;
-    Title* next;
-    Title* alloc(ull vtitle, Title* vnext)
-    {
-        title = vtitle;
-        next = vnext;
-        return this;
-    }
-}buf[MAXTITLE],*htable[MAXTITLE];
-
-int tmpIdx;
-int AuthorIdx[MAXAUTHOR];
-int GenreIdx[MAXGENRE];
-
-Node Book[MAXTITLE];
-
-
-int findTitleidx(char name[])
-{
-
-}
-int findAuthorIdx(char name[])
-{
-    
-}
-
-int findGenreIdx(char name[])
-{
-
-}
-
-
-int bigyo(Node a, Node b)
-{
-    if (a.loan > b.loan)
-    {
-        return 1;
-    }
-    else if (a.loan == b.loan)
-    {
-        if (strcmp(a.title, b.title) < 0)
-            return 1;
-        return 0;
-    }
-    else
-    {
-        return 0;
-    }
-}
-void swap(Node& a, Node& b)
-{
-    Node tmp = a;
-    a = b;
-    b = tmp;
-}
-struct PriorityQueue
-{
-    Node heap[MAXTITLE];
-    int (*cmp)(Node, Node);
-    int bn;
-
-    void init()
-    {
-        cmp = bigyo;
-        bn = 0;
-    }
-
-    void push(Node num)
-    {
-        heap[++bn] = num;
-
-        for (int c = bn;c > 1;c /= 2)
-        {
-            if (c < bn && cmp(heap[c+1], heap[c]))
-            {
-                c++;
-            }
-            if (cmp(heap[c], heap[c / 2]))
-            {
-                swap(heap[c], heap[c / 2]);
-            }
-        }
-    }
-
-
-}pqAuthor[MAXAUTHOR],pqGenre[MAXGENRE];
-
+#define MAXIDX MAXTITLE+MAXAUTHOR+MAXGENRE
 
 int strcmp(const char* s, const char* t) {
     while (*s && *s == *t) ++s, ++t;
@@ -117,12 +27,212 @@ void strcpy(char* dest, const char* src) {
     while ((*dest++ = *src++));
 }
 
-void init() {
-    tmpIdx = 0;
+struct Book
+{
+    char title[MAXTITLELEN];
+    char author[MAXAUTHORLEN];
+    char genre[MAXGENRELEN];
+    int loanCnt;
+    int remainCnt;
+
+}bookInfo[MAXTITLE];
+
+
+struct Title
+{
+    char title[MAXTITLELEN];
+    int idx;
+    Title* next;
+    Book* here;
+
+    Title* alloc(char vtitle[], int vidx, Title* vnext,Book *vhere)
+    {
+        strcpy(title, vtitle);
+        idx = vidx;
+        next = vnext;
+        here = vhere;
+        return this;
+    }
+}bufTitle[MAXTITLE], titleTable[MAXTITLE];
+
+int tmpTitle;
+
+struct Author
+{
+    char author[MAXAUTHORLEN];
+    int idx;
+    Author* next;
+
+    Author* alloc(char vauthor[], int vidx, Author* vnext)
+    {
+        strcpy(author, vauthor);
+        idx = vidx;
+        next = vnext;
+        return this;
+    }
+}bufAuthor[MAXAUTHOR], authorTable[MAXAUTHOR];
+
+int tmpAuthor;
+
+struct Genre
+{
+    char genre[MAXGENRELEN];
+    int idx;
+    Genre* next;
+
+    Genre* alloc(char vgenre[], int vidx, Genre* vnext)
+    {
+        strcpy(genre, vgenre);
+        idx = vidx;
+        next = vnext;
+        return this;
+    }
+}bufGenre[MAXGENRE],genreTable[MAXGENRE];
+
+int tmpGenre;
+
+
+int tmpIdx;
+
+int authorCnt[MAXIDX];
+int genreCnt[MAXIDX];
+
+int getHashTitle(char title[]) 
+{
+    int code = 5381;
+    for (int i = 0; title[i]; i++)
+    {
+        code = (code * 33 + title[i]) % MAXTITLE;
+    }
+    return code;
 }
+
+int getHashAuthor(char author[])
+{
+    int code = 5381;
+    for (int i = 0; author[i]; i++)
+    {
+        code = (code * 33 + author[i]) % MAXAUTHOR;
+    }
+    return code;
+}
+
+int getHashGenre(char genre[])
+{
+    int code = 5381;
+    for (int i = 0; genre[i]; i++)
+    {
+        code = (code * 33 + genre[i]) % MAXGENRE;
+    }
+    return code;
+}
+
+void init() {
+
+    for (int i = 0; i < MAXTITLE; i++)
+    {
+        titleTable[i].next = NULL;
+        bookInfo[i].remainCnt = 0;
+        bookInfo[i].loanCnt = 0;
+    }
+    for (int i = 0; i < MAXAUTHOR; i++)
+    {
+        authorTable[i].next = NULL;
+    }
+    for (int i = 0; i < MAXGENRE; i++)
+    {
+        genreTable[i].next = NULL;
+    }
+
+    tmpTitle = tmpAuthor = tmpGenre = tmpIdx = 0;
+
+    for (int i = 0; i < MAXIDX; i++)
+    {
+        authorCnt[i] = genreCnt[i] = 0;
+    }
+}
+
+int findBookIdx(char title[])
+{
+    int titleKey = getHashTitle(title);
+
+    Title* p = titleTable[titleKey].next;
+
+    for (; p; p = p->next)
+    {
+        if (strcmp(title, p->title) == 0)
+        {
+            return p->idx;
+        }
+    }
+    
+
+    titleTable[titleKey].next = bufTitle[tmpTitle].alloc(title, tmpTitle, titleTable[titleKey].next, &bookInfo[tmpTitle]);
+    tmpTitle++;
+    return tmpTitle - 1;
+}
+
+int findAuthorIdx(char author[])
+{
+    int key = getHashAuthor(author);
+
+    Author* p = authorTable[key].next;
+
+    for (; p; p = p->next)
+    {
+        if (strcmp(p->author, author) == 0)
+            return p->idx;
+    }
+
+    authorTable[key].next = bufAuthor[tmpAuthor].alloc(author, tmpAuthor, authorTable[key].next);
+    tmpAuthor++;
+    return tmpAuthor - 1;
+}
+
+int findGenreIdx(char genre[])
+{
+    int key = getHashGenre(genre);
+
+    Genre* p = genreTable[key].next;
+
+    for (; p; p = p->next)
+    {
+        if (strcmp(p->genre, genre) == 0)
+            return p->idx;
+    }
+
+    genreTable[key].next = bufGenre[tmpGenre].alloc(genre, tmpGenre, genreTable[key].next);
+    tmpGenre++;
+    return tmpGenre - 1;
+}
+
+
+
+
+struct PriorityQueue
+{
+    //지은이랑 장르.. IDX이용해서 힙 업데이트도 구현해야해
+
+    
+};
+
 
 void inputBook(char title[], char author[], char genre[], int cnt) {
 
+    int titleIdx = findBookIdx(title);
+    Book me = bookInfo[titleIdx];
+    strcpy(bookInfo[titleIdx].title, title);
+    strcpy(bookInfo[titleIdx].author, author);
+    strcpy(bookInfo[titleIdx].genre, genre);
+    bookInfo[titleIdx].remainCnt += cnt;
+
+    int authorIdx = findAuthorIdx(author);
+    authorCnt[authorIdx] += cnt;
+    int genreIdx = findGenreIdx(genre);
+    genreCnt[genreIdx] += cnt;
+
+
+    //pq에도 추가
 }
 
 void loanBook(char title[], int cnt) {
